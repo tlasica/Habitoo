94362e245a329f82eb2f30fad9edaa98363f7df7
Tomasz Łasica <tlasica@o2.pl>
2013-06-16 23:06:01 +0200

    Top panel with current date

--- src/pl/tlasica/keepdoing/MainActivity.java
+++ src/pl/tlasica/keepdoing/MainActivity.java
@@ -1,15 +1,32 @@
 package pl.tlasica.keepdoing;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import pl.tlasica.keepdoing.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.text.format.DateFormat;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private Calendar currDay = Calendar.getInstance();
+	private TextView m_CurrDayText;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
+		m_CurrDayText = (TextView) findViewById(R.id.textview_date);
+		
+		updateContent();
     }
 
     @Override
@@ -18,5 +35,31 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+ 
+    public void previousDay(View view) {
+    	currDay.add(Calendar.DAY_OF_YEAR, +1);
+    	updateContent();    	    	
+    }
     
+    public void nextDay(View view) {
+    	currDay.add(Calendar.DAY_OF_YEAR, +1);
+    	updateContent();
+    }
+    
+    public void today(View view) {
+    	currDay = Calendar.getInstance();
+    	updateContent();
+    }
+    
+    private void updateContent() {
+    	updateCurrDay();
+//    	loadDataFromDatabase();
+    	//updateCommitments();
+    }
+
+	private void updateCurrDay() {
+		String str = DateFormat.getDateFormat(getApplicationContext()).format( currDay.getTime() );
+		m_CurrDayText.setText( str );
+	}
+	
 }
